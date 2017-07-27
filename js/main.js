2bc8421d5a65bf5b367df3e9199cc79a062736b6
Coryn Vong <coryn.vong@gmail.com>
2017-07-27 11:18:46 -0700

    Fix indentation

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 const catFirstName = {
-            'A' : 'Apple',
-            'B' : 'Berry',
-            'C' : 'Creek',
-            'D' : 'Dawn',
-            'J' : 'Jay',
-            'K' : 'Killer'
-        };
+    'A' : 'Apple',
+    'B' : 'Berry',
+    'C' : 'Creek',
+    'D' : 'Dawn',
+    'J' : 'Jay',
+    'K' : 'Killer'
+};
 
-        const catLastName = {
-            'F' : 'Fluffy',
-            'V' : 'Victory'
-        };
+const catLastName = {
+    'F' : 'Fluffy',
+    'V' : 'Victory'
+};
 
-        const generator = document.querySelector('form');
+const generator = document.querySelector('form');
 
-        generator.addEventListener('submit', generate);
+generator.addEventListener('submit', generate);
 
-        function generate(e) {
-            e.preventDefault();
+function generate(e) {
+    e.preventDefault();
 
-            let firstName = document.querySelector('#first-name').value;
-            let lastName = document.querySelector('#last-name').value;
+    let firstName = document.querySelector('#first-name').value;
+    let lastName = document.querySelector('#last-name').value;
 
-            const firstInitial = firstName.substring(0, 1).toUpperCase();
-            const lastInitial = lastName.substring(0, 1).toUpperCase();
+    const firstInitial = firstName.substring(0, 1).toUpperCase();
+    const lastInitial = lastName.substring(0, 1).toUpperCase();
 
-            alert('Your warrior cat name is ' + catFirstName[firstInitial] + ' ' + catLastName[lastInitial]);
+    alert('Your warrior cat name is ' + catFirstName[firstInitial] + ' ' + catLastName[lastInitial]);
 
-            document.querySelector('#first-name').value = '';
-            document.querySelector('#last-name').value = '';
-        }
+    document.querySelector('#first-name').value = '';
+    document.querySelector('#last-name').value = '';
+}
