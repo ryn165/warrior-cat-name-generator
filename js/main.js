e56f3414a623f0c5b75f69b6a6710dee838a2123
Coryn Vong <coryn.vong@gmail.com>
2017-07-27 11:14:30 -0700

    Move js to own file

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const catFirstName = {
+            'A' : 'Apple',
+            'B' : 'Berry',
+            'C' : 'Creek',
+            'D' : 'Dawn',
+            'J' : 'Jay',
+            'K' : 'Killer'
+        };
+
+        const catLastName = {
+            'F' : 'Fluffy',
+            'V' : 'Victory'
+        };
+
+        const generator = document.querySelector('form');
+
+        generator.addEventListener('submit', generate);
+
+        function generate(e) {
+            e.preventDefault();
+
+            let firstName = document.querySelector('#first-name').value;
+            let lastName = document.querySelector('#last-name').value;
+
+            const firstInitial = firstName.substring(0, 1).toUpperCase();
+            const lastInitial = lastName.substring(0, 1).toUpperCase();
+
+            alert('Your warrior cat name is ' + catFirstName[firstInitial] + ' ' + catLastName[lastInitial]);
+
+            document.querySelector('#first-name').value = '';
+            document.querySelector('#last-name').value = '';
+        }
